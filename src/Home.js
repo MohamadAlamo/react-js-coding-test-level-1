0dfe1f662dfbd726b56d82eea066483804f7961d
Mohamad alamo <Mohammed_alamo@hotmail.com>
2022-06-27 16:41:46 +0800

    First Test Done

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,50 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-
+// "http://localhost:3000/#/pokedex"
 function Home() {
-  const [text, setText] = useState("");
-  const [isReady, setIsReady] = useState(false);
+  const [text, setText] = useState();
+  const [isReady, setIsReady] = useState();
+  const [isReadyToClick, setIsReadyToClick] = useState();
+  function onClick() {
+    if (isReady) {
+      window.location = "http://localhost:3000/#/pokedex";
+    }
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          hidden={!isReady}
-          src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
-          className="App-logo"
-          alt="logo"
-          style={{ padding: "10px" }}
-        />
+        <div onClick={onClick}>
+          <img
+            hidden={false}
+            src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
+            className="App-logo"
+            alt="logo"
+            style={{ padding: "10px" }}
+          />
+        </div>
+
         <b>
           Requirement: Try to show the hidden image and make it clickable that
           goes to /pokedex when the input below is "Ready!" remember to hide the
           red text away when "Ready!" is in the textbox.
         </b>
         <p>Are you ready to be a pokemon master?</p>
-        <input type="text" name="name" />
-        <span style={{ color: "red" }}>I am not ready yet!</span>
+        <input
+          type="text"
+          name="name"
+          value={text}
+          onKeyUp={setIsReady}
+          onChangeText={(text) => {
+            setText(text);
+          }}
+        />
+        {!isReady ? (
+          <span style={{ color: "red" }}>I am not ready yet!</span>
+        ) : (
+          <span style={{ color: "white" }}>I am ready</span>
+        )}
       </header>
     </div>
   );
